27f4adfb0e9b7d1d7758f6d1d5665c71d08ab751
Ryan Kristopher Anderson <78626914+ryan-k-anderson@users.noreply.github.com>
2023-01-19 19:54:14 -0500

    Fixed textReader.js and uncommented actual code

--- textReader.js
+++ textReader.js
@@ -5,51 +5,53 @@ const fileName = 'loremipsum.txt';
 const gzip = zlib.createGzip();
 
 //Synchronous Version
-// const text = fs.readFileSync(fileName, 'utf8');
+const text = fs.readFileSync(fileName, 'utf8');
     //fs.readeFileName is a function in the fs module that will read specified files you tell it to
     //utf8 is the encoding needed to bring back what is needed
 
-// console.log(text)
+console.log(text)
 
 
 
 //Asynchronous Version
-// fs.readFile(fileName, 'utf8', (err, data) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log(data)
-// });
+fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    console.log(data)
+});
 
-// console.log('This will get logged to console first')
+console.log('This will get logged to console first')
 
 
 
 //Readable Stream Version
-// const readStream = fs.createReadStream(fileName, 'utf8');
-// const writeStream = fs.createWriteStream('output.txt', 'utf8');
-// let myData = '';
-// let totalChunks = 0;
+let readStream = fs.createReadStream(fileName, 'utf8');
+let writeStream = fs.createWriteStream('output.txt', 'utf8');
+let myData = '';
+let totalChunks = 0;
 
-// readStream.on('data', (chunk) => {
-// myData += chunk;
-// totalChunks +=1;
-// console.log(totalChunks)
-// writeStream.write(`Chunk#${totalChunks}: ${chunk}`)
-// });
+readStream.on('data', (chunk) => {
+myData += chunk;
+totalChunks +=1;
+console.log(totalChunks)
+writeStream.write(`Chunk#${totalChunks}: ${chunk}`)
+});
 
-// readStream.on('end', () => {
-//     console.log('Finished reading data')
-// });
+readStream.on('end', () => {
+    console.log('Finished reading data')
+});
 
-// readStream.on('error', (err) => {
-//     throw err;
-// });
+readStream.on('error', (err) => {
+    throw err;
+});
 
 
 
 //Compressing into a zip file
-const readStream = fs.createReadStream(fileName, 'utf8');
-const writeStream = fs.createWriteStream('output.txt.gz', 'utf8');
+let readStream = fs.createReadStream(fileName, 'utf8');
+let writeStream = fs.createWriteStream('output.txt.gz', 'utf8');
 
 readStream.pipe(gzip).pipe(writeStream)
+
+//This code as is will not work. Comment out the parts not needed
